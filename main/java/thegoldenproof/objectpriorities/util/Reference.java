9428360ba70b437f0b66201f404ca87d07740bcb
TheGoldenProof <firebirdspiral@gmail.com>
2019-01-21 15:23:41 -0600

    updation

--- main/java/thegoldenproof/objectpriorities/util/Reference.java
+++ main/java/thegoldenproof/objectpriorities/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "objectpriorities";
 	public static final String NAME = "ObjectPriorities";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.1.2";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "thegoldenproof.objectpriorities.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "thegoldenproof.objectpriorities.proxy.ServerProxy";
